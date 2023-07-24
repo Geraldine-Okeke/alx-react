f34c2a015f1d9d20932c4329051cf688ebd2571d
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-07-24 21:20:26 +0100

    task3

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,21 +1,18 @@
-// js/dashboard_main.js
+import '../css/main.css';
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let conunt = 0;
 
 function updateCounter() {
-  const countParagraph = $('#count');
-  let count = parseInt(countParagraph.text()) || 0;
-  count += 1;
-  countParagraph.text(`${count} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-$(document).ready(function () {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="clickBtn">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  const clickButton = $('#clickBtn');
-  clickButton.on('click', debounce(updateCounter, 1000));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,27 +1,34 @@
-// webpack.config.js
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
-  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
+        test: /\.(gif|png|jp?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: 'file-loader',
+            loader: "image-webpack-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
+              bypassOnDebug: true,
+              disable: true,
             },
           },
         ],
